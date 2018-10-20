0e07f16f0a20d0b2a243438bcccd2c543d7e5e24
David Adeyemi <davidadeyemi@my.unt.edu>
2018-10-20 18:34:35 -0500

    Add Toggle Functionality, among other Optional Updates

--- app/src/main/java/com/example/david/flashme/MainActivity.java
+++ app/src/main/java/com/example/david/flashme/MainActivity.java
@@ -6,17 +6,20 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    boolean isShowingAnswers = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
                 findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
+                //findViewById(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                //using card_background instead
 
             }
         });
@@ -26,6 +29,47 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                     findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
                     findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
+                    //findViewById(R.id.flashcard_question).setBackgroundColor(getResources().getColor(R.color.colorAccent));
+            }
+        });
+
+        //on click listener that sets correct color, while wrong color goes red.
+        findViewById(R.id.answer1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer1).setBackgroundColor(getResources().getColor(R.color.rubyRed));
+                findViewById(R.id.answer2).setBackgroundColor(getResources().getColor(R.color.deepGreen));
+            }
+        });
+        findViewById(R.id.answer3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer3).setBackgroundColor(getResources().getColor(R.color.rubyRed));
+                findViewById(R.id.answer2).setBackgroundColor(getResources().getColor(R.color.deepGreen));
+            }
+        });
+        findViewById(R.id.answer2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.answer2).setBackgroundColor(getResources().getColor(R.color.deepGreen));
+            }
+        });
+        findViewById(R.id.toggle_choices_visibility).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.hide_icon_a);
+                if (isShowingAnswers == true) {
+                    findViewById(R.id.answer1).setVisibility(View.VISIBLE);
+                    findViewById(R.id.answer2).setVisibility(View.VISIBLE);
+                    findViewById(R.id.answer3).setVisibility(View.VISIBLE);
+                    isShowingAnswers = false;
+                }
+                else {
+                    findViewById(R.id.answer1).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.answer2).setVisibility(View.INVISIBLE);
+                    findViewById(R.id.answer3).setVisibility(View.INVISIBLE);
+                    isShowingAnswers = true;
+                }
             }
         });
 
