45406b2ec553afadc314fa4469dd9f8c4ba21ee9
David Adeyemi <davidadeyemi@my.unt.edu>
2018-10-20 19:23:37 -0500

    Color Palette Changes as well as mainactivity comments

--- app/src/main/java/com/example/david/flashme/MainActivity.java
+++ app/src/main/java/com/example/david/flashme/MainActivity.java
@@ -5,35 +5,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    //toggle value for showing answer list
     boolean isShowingAnswers = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+    // First on click listener to toggle between flashcard answer and question
         findViewById(R.id.flashcard_question).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 findViewById(R.id.flashcard_question).setVisibility(View.INVISIBLE);
                 findViewById(R.id.flashcard_answer).setVisibility(View.VISIBLE);
-                //findViewById(R.id.flashcard_answer).setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-                //using card_background instead
+
 
             }
         });
-        //second on click listener to toggle between views
+        //second on click listener to toggle between flashcard answer and question
         findViewById(R.id.flashcard_answer).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                     findViewById(R.id.flashcard_question).setVisibility(View.VISIBLE);
                     findViewById(R.id.flashcard_answer).setVisibility(View.INVISIBLE);
-                    //findViewById(R.id.flashcard_question).setBackgroundColor(getResources().getColor(R.color.colorAccent));
+
             }
         });
 
-        //on click listener that sets correct color, while wrong color goes red.
+        //on click listeners that set correct color, while wrong color goes red.
         findViewById(R.id.answer1).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,21 +53,23 @@ public class MainActivity extends AppCompatActivity {
                 findViewById(R.id.answer2).setBackgroundColor(getResources().getColor(R.color.deepGreen));
             }
         });
+
         findViewById(R.id.toggle_choices_visibility).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.hide_icon_a);
-                if (isShowingAnswers == true) {
+                //((ImageView) findViewById(R.id.toggle_choices_visibility)).setImageResource(R.drawable.hide_icon_a); will be useful for changing icon later
+                //logic for toggling visibility of the answer list
+                if (isShowingAnswers) {
                     findViewById(R.id.answer1).setVisibility(View.VISIBLE);
                     findViewById(R.id.answer2).setVisibility(View.VISIBLE);
                     findViewById(R.id.answer3).setVisibility(View.VISIBLE);
-                    isShowingAnswers = false;
+                    isShowingAnswers = !isShowingAnswers;
                 }
                 else {
                     findViewById(R.id.answer1).setVisibility(View.INVISIBLE);
                     findViewById(R.id.answer2).setVisibility(View.INVISIBLE);
                     findViewById(R.id.answer3).setVisibility(View.INVISIBLE);
-                    isShowingAnswers = true;
+                    isShowingAnswers = !isShowingAnswers;
                 }
             }
         });
